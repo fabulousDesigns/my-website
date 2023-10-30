98e1a5240b8f53649465e99393244b55a8aa5772
Bernard Maina <bmburu@innovexsolutions.co.ke>
2023-10-30 08:39:15 +0300

    Updates

---
+++ precache-manifest.42a4c185d9aa0dee6affa41dcc352b3d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff2f985c98395a4b80b6dc6dd176d42",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "5b04f781afcccc3f72ed",
+    "url": "/my-website/static/css/main.845e4171.chunk.css"
+  },
+  {
+    "revision": "db58d7707f24023ce35f",
+    "url": "/my-website/static/js/2.51316fe7.chunk.js"
+  },
+  {
+    "revision": "5b04f781afcccc3f72ed",
+    "url": "/my-website/static/js/main.e92c4687.chunk.js"
+  },
+  {
+    "revision": "37063c54184f9fd8244e",
+    "url": "/my-website/static/js/runtime~main.99d9029d.js"
+  },
+  {
+    "revision": "7b3accc9a50fb3d5d4678f226e32fbd0",
+    "url": "/my-website/static/media/img-1.7b3accc9.png"
+  },
+  {
+    "revision": "2a9d3d04a7d14824535d44501f7899b9",
+    "url": "/my-website/static/media/portfolio1.2a9d3d04.png"
+  },
+  {
+    "revision": "f74fcc0590e8eab3a4be6986ae669233",
+    "url": "/my-website/static/media/portfolio2.f74fcc05.png"
+  },
+  {
+    "revision": "d6cd83d9d45bc5bd17946d97a0b132c7",
+    "url": "/my-website/static/media/portfolio3.d6cd83d9.png"
+  },
+  {
+    "revision": "9da62c8ecc541b68e7c5c927d83d310b",
+    "url": "/my-website/static/media/portfolio4.9da62c8e.png"
+  },
+  {
+    "revision": "d6a4832f7381dae02a53b557789312be",
+    "url": "/my-website/static/media/portfolio5.d6a4832f.png"
+  },
+  {
+    "revision": "54f0f89dc50b80822e74a45204fd8c5d",
+    "url": "/my-website/static/media/portfolio6.54f0f89d.png"
+  },
+  {
+    "revision": "f947a279289468df7a513968cf002e31",
+    "url": "/my-website/static/media/resume.f947a279.pdf"
+  }
+]);
